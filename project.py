64a6b8323f0aa00d6a0b149a05edb399909cb8de
Michal Petrán <progamingstudiohd@gmail.com>
2024-03-02 10:49:41 +0100

    Read and write a simple file
    The functionality to read a simple questions file and write/rewrite it.
    Start of the game and choosing mode

---
+++ project.py
@@ -0,0 +1,89 @@
+# TODO: 1. Quiz, retry, delete, write 
+#           answers to a file
+#       2. Timer, print out final score
+#       3. other functionality, mnultiple answers
+
+import sys
+import re
+import csv
+
+COUNTER = 0
+
+def main():
+   game_selector()
+    
+def game_selector():
+    print("Welcome to the Quiz Game!")
+    print("1. Start a new game")
+    print("2. Quit")
+    try:
+        choice = int(input("Enter your choice: ").strip())
+    except ValueError:
+        print("You did not input a number!")
+        pass
+    if choice == 1:
+        game_manager(input("Enter the type of game you want to play(simple quiz | hard quiz): ").strip())
+    elif choice == 2:
+        sys.exit()
+    else:
+        print("Invalid choice, please try again.")
+        game_selector()
+ 
+
+def game_manager(game_type):
+    if re.search("^(s|si|sim|simp|simpl|simple)$", game_type):
+        print("Simple quiz selected")
+        simple_game()
+    if re.search("^(h|ha|har|hard)$", game_type):
+        print("Hard quiz selected")
+        hard_game()
+        
+def simple_game():
+    
+def hard_game():
+
+def read_sf(file_name):
+    question = []
+    try:
+        with open(file_name, 'r') as sfr:
+            reader = csv.DictReader(sfr)
+            for row in reader:
+                question.append({"question": row["question"], "answer": row["answer"]})
+            return question
+    except FileNotFoundError:
+        answer = input("File not found, would you like to create it?(yes, no): ").strip().lower()
+        if answer == "yes":
+            write_sf(file_name)
+        else:
+            sys.exit()
+    
+
+def write_sf(file_name):
+    with open(file_name, "w", newline="") as sfw:
+        writer = csv.DictWriter(sfw, fieldnames=["question", "answer"])
+        writer.writeheader()
+        for question in ask_question():
+            writer.writerow({"question": question["question"], "answer": question["answer"]})
+         
+    
+def ask_question():
+    print("\nYou will now be tasked with writing the questions and the answers. If you wish to stop type end.")
+    print("Do not end without finishing the question and answer set!\n")
+    questions_answers = []
+    while True:
+        user_question = input("Enter a question: ").strip()
+        if user_question == "end":
+            break
+        user_answer = input("Enter the answer: ").strip()
+        if user_answer == "end":
+            print("You cannot end without finishing the question and answer set!")
+            continue
+        questions_answers.append({"question": user_question, "answer": user_answer})
+    final_set = remove_duplicates(questions_answers)
+    return final_set
+
+def remove_duplicates(data):
+    return [dict(t) for t in set(tuple(d.items()) for d in data)]
+    
+if __name__ == "__main__":
+    main()
